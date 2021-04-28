7059ea3d0fea0dcea917b2a0e1ced4f1f4b40534
DavidDGTNTSchool <>
2021-04-28 18:28:14 +0000

    Built and applied subfolder fix

---
+++ build/precache-manifest.087afa5b7a88100676aea8b8e74eb1cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e122985ad9120e5a0da360cd7c8e1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e2d2f370dd62c6173f7",
+    "url": "/static/css/main.cba80d84.chunk.css"
+  },
+  {
+    "revision": "7d264d5ea7bf5a8c2892",
+    "url": "/static/js/2.00d17935.chunk.js"
+  },
+  {
+    "revision": "8e2d2f370dd62c6173f7",
+    "url": "/static/js/main.21cf5b63.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
